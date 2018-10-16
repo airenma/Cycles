a418460fcc9a3df98fddb9ba205157f6f721e2cb
airenma <airenma@gmail.com>
2018-10-16 15:34:43 +0300

    cycles

---
+++ Program.java
@@ -0,0 +1,34 @@
+
+public class Program{
+	public static void main(String[] args)
+	{
+		int i = 0;
+		while(i<5)
+		{
+			System.out.println("i = " + i);
+			i++;
+		}
+
+		do {
+				System.out.println(i);
+				i--;
+			}
+			while(i>0);
+
+		
+
+		int num = 4;
+			while (num >= 0)
+			{
+			System.out.println(num + " is num");
+			num--;
+			}
+
+			do {
+				num ++;
+				System.out.println(num);
+			}while(num < 4);
+
+		
+	}
+}
